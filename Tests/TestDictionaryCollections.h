0d33cd0d8519da67580891dcb5015ee18c25fae9
Michael Dippery <mdippery@gmail.com>
2011-01-31 16:01:31 -0500

    Implement Smalltalk-ish methods for NSDictionary

---
+++ Tests/TestDictionaryCollections.h
@@ -0,0 +1,9 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TestDictionaryCollections : SenTestCase
+{
+    NSDictionary *postalCodes;
+    NSDictionary *eats;
+}
+@end
---
+++ Tests/TestDictionaryCollections.m
@@ -0,0 +1,93 @@
+#import "TestDictionaryCollections.h"
+#import "NSDictionaryCollections.h"
+
+#include <ctype.h>
+
+
+@implementation TestDictionaryCollections
+
+- (void)setUp
+{
+    NSArray *animals = [NSArray arrayWithObjects:@"anteaters", @"humans", @"deer", @"owls", nil];
+    NSArray *food = [NSArray arrayWithObjects:@"ants", @"anything", @"plants", @"mice", nil];
+    NSArray *states = [NSArray arrayWithObjects:@"Pennsylvania", @"New York", @"Virginia", nil];
+    NSArray *codes = [NSArray arrayWithObjects:@"pa", @"ny", @"va", nil];
+    eats = [[NSDictionary alloc] initWithObjects:food forKeys:animals];
+    postalCodes = [[NSDictionary alloc] initWithObjects:codes forKeys:states];
+}
+
+- (void)testDo
+{
+    NSString *target = @"I have lived in X, X, and X.";
+    __block NSMutableString *places = [NSMutableString stringWithString:@"I have lived in"];
+    __block int i = 1;
+    NSArray *states = [NSArray arrayWithObjects:@"Pennsylvania", @"New York", @"Virginia", nil];
+    NSArray *abbrevs = [NSArray arrayWithObjects:@"PA", @"NY", @"VA", nil];
+    NSDictionary *data = [NSDictionary dictionaryWithObjects:abbrevs forKeys:states];
+    [data do:^ (id obj) {
+        if (i == 1) {
+            [places appendString:@" X"];
+        } else if (i == 2) {
+            [places appendString:@", X"];
+        } else {
+            [places appendString:@", and X."];
+        }
+        i++;
+    }];
+    STAssertTrue([places isEqualToString:target], @"String should be '%@', is '%@'", target, places);
+}
+
+- (void)testCollect
+{
+    NSDictionary *d = [postalCodes collect:^ id (id s) { return [s uppercaseString]; }];
+    for (NSString *key in d) {
+        NSString *code = [d objectForKey:key];
+        for (NSUInteger i = 0U; i < [code length]; i++) {
+            STAssertTrue(isupper([code characterAtIndex:i]), @"'%@' does not consist entirely of uppercase letters", code);
+        }
+    }
+}
+
+- (void)testInject
+{
+    NSString *s = @"I have lived in: ";
+    __block BOOL first = YES;
+    NSString *sout = [postalCodes inject:s into:^ id (id memo, id obj) {
+        if (!first) memo = [memo stringByAppendingString:@", "];
+        memo = [memo stringByAppendingString:obj];
+        first = NO;
+        return memo;
+    }];
+    STAssertEquals([sout length], (NSUInteger) 27U, @"Output is '%@'", sout);
+}
+
+- (void)testReject
+{
+    NSSet *targets = [NSSet setWithObjects:@"ants", @"anything", nil];
+    NSDictionary *d = [eats reject:^ BOOL (id obj) { return ![obj hasPrefix:@"a"]; }];
+    STAssertEquals([d count], [targets count], @"Dictionary should have %u elements, has %u", [targets count], [d count]);
+    for (NSString *key in d) {
+        NSString *food = [d objectForKey:key];
+        STAssertTrue([targets containsObject:food], @"target should contain the object %@", food);
+    }
+}
+
+- (void)testSelect
+{
+    NSSet *targets = [NSSet setWithObjects:@"ants", @"anything", nil];
+    NSDictionary *d = [eats select:^ BOOL (id obj) { return [obj hasPrefix:@"a"]; }];
+    STAssertEquals([d count], [targets count], @"Dictionary should have %u elements, has %u", [targets count], [d count]);
+    for (NSString *key in d) {
+        NSString *food = [d objectForKey:key];
+        STAssertTrue([targets containsObject:food], @"target should contain the object %@", food);
+    }
+}
+
+
+- (void)tearDown
+{
+    [postalCodes release]; postalCodes = nil;
+    [eats release]; eats = nil;
+}
+
+@end
